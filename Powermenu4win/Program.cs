a2e70b29a0169a93e20e49c49d7fbbb66fa0d618
libor <litszwaiboris@yahoo.com>
2022-01-16 12:16:31 +0800

    Initial commit

---
+++ Powermenu4win/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Powermenu4win
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Select an option to proceed\n");
+            Console.WriteLine("a. Shutdown\n");
+            Console.WriteLine("b. Reboot\n");
+            Console.WriteLine("c. Cancel");
+            string input = Console.ReadLine();
+            if (input == "a")
+            {
+                string cmdexec = "/c shutdown -s -t 0";
+                System.Diagnostics.Process.Start("cmd.exe", cmdexec);
+            }
+            if (input == "b")
+            {
+                string cmdexec = "/c shutdown -r -t 0";
+                System.Diagnostics.Process.Start("cmd.exe", cmdexec);
+            }
+            if (input == "c")
+            {
+                Environment.Exit(0);
+            }
+        }
+    }
+}
